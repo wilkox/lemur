81bc4593b5fc6d77122a8d8e6762813ce5d187df
David Wilkins <david@wilkox.org>
2024-10-08 13:18:29 +1030

    Store OpenAI API key after first retrieval

--- R/openai-api.R
+++ R/openai-api.R
@@ -1,11 +1,18 @@
 #' Retrieve OpenAI API key from the OPENAI_API_KEY environmental variable
 #'
 openai_api_key <- function() {
+
+  # Check if already set in options
+  if (! is.null(getOption("lemur.OPENAI_API_KEY"))) {
+    return(getOption("lemur.OPENAI_API_KEY"))
+  }
+
   cli::cli_alert_info("Retrieving OpenAI API key from {.envvar OPENAI_API_KEY} environmental variable...")
   openai_api_key <- Sys.getenv("OPENAI_API_KEY")
   if (stringr::str_length(openai_api_key) == 0 | is.na(openai_api_key)) {
     cli::cli_abort("Cannot find environmental variable {.envvar OPENAI_API_KEY}")
   }
+  options(`lemur.OPENAI_API_KEY` = openai_api_key)
   openai_api_key
 }
 
